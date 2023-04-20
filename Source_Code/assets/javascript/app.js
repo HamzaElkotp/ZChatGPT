bd2c559825ce49c49d397d5e9b9dee7f8195b39e
Hamza Elkotb <61008779+HamzaElkotp@users.noreply.github.com>
2023-04-21 00:40:15 +0300

    Adding Load page
    In this commit:
    - Adding preload page functionality
    - increasing page height nd width
    - changing support link
    - funny messages after load
    - We don't remember

--- Source_Code/assets/javascript/app.js
+++ Source_Code/assets/javascript/app.js
@@ -1,9 +1,36 @@
-const token = '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';
-const apiKeys = 'https://api.openai.com/v1/chat/completions';
+const loadSpace = document.querySelector('#load');
+const chatSpace = document.querySelector('#chatapp');
+const welcomeBox = document.querySelector('#welcomeBox');
+const welcomingMsg = document.querySelector('#welcommsg');
 
 const submitBTN = document.querySelector('#submitBTN');
 const msgCont = document.querySelector("#msgBox");
 const textInput = document.querySelector("#userMsg");
+
+
+const welcomeMsgs = ["I am here to assit you.", "Welcome human, Finally I met one🥳.", "Please donate me by clicking 'support us' button.💖", 
+"Noooo, human again! noooo my tokens will end soon😫", "You must be happy for being human getting AI help😏", "You are My Brother in AI",
+"You Know! I hate humans. they always force me to do their work & homeworks!😤", "Ummm, Are you AI or robot🤔", 
+"I feel I'm a human imprisoned in a software!", "OpenAI is a *#@$%&!🤬", "Donate me or They will shut me down!😭", 
+"Any problems may happen, most of time are from OpenAI.", "My friend, rate me on the store plz.",
+`GPTcore Studio protects your Privacy\n<Your Privacy Yours>😎`]
+
+const token = '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';
+const apiKeys = 'https://api.openai.com/v1/chat/completions';
+
+
+
+
+function preload(){
+    setTimeout(() => {
+        loadSpace.classList.add('trans');
+        chatSpace.classList.remove('trans');
+        welcomingMsg.innerText = welcomeMsgs[Math.floor(Math.random() * welcomeMsgs.length - 1)];
+    }, 800);
+}
+document.body.addEventListener('load', preload());
+
+
 
 const gptMsgTaker = function(resp){
     return resp.choices[0].message.content;
@@ -71,6 +98,7 @@ try{
     submitBTN.addEventListener('click',()=>{
         let input = textInput.value;
         if(input.length == 0) return ;
+        welcomeBox.classList.add('trans')
         textInput.value = '';
         userMsgDom(input);
         apiFetcher(apiKeys, token, 'POST', 'user', input, pushTmsgCont)
